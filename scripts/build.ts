17bd7651668aefb5e8c6a2de8b71867bebef870f
Wiz1991 <wizoftime92@gmail.com>
2022-05-19 12:06:01 +0200

    fix build, update fadroma

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,25 @@
-import { Source } from '@hackbg/fadroma';
+import { print, Source } from '@hackbg/fadroma';
 import { resolve, dirname } from 'path';
 import { existsSync, readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const workspace = dirname(__dirname);
-
 export const contracts = ['multicall'];
+export const versions = {
+    HEAD: 'HEAD',
+};
+
+export const __dirname = dirname(fileURLToPath(import.meta.url));
+export const workspace = dirname(__dirname);
 
 export function source(crate: string, ref = 'HEAD') {
-    return new Source(dirname(__dirname), crate, ref);
+    return new Source(workspace, crate, ref);
 }
 export function sources(ref: any, ...crateLists: any[]) {
+    console.log(workspace);
     return Source.collect(workspace, ref, ...crateLists);
 }
+
+const build = {};
+export default build;
+
+build['multicall'] = () => sources(versions.HEAD, contracts);
